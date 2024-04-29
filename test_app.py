d6629b91836c5025bf579d0d64a69f183fd92501
JacobisEpic <108195485+JacobisEpic@users.noreply.github.com>
2024-04-28 23:01:06 -0400

    Unit Testing

--- test_app.py
+++ test_app.py
@@ -1,7 +1,15 @@
 import pytest
-from app import create_app
-from flask import session
+from app import app as flask_app
 
 @pytest.fixture
-def register(client, username, password):
-    return client.post('/register', data={'username': username, 'password': password}, follow_redirects=True)
+def app():
+    yield flask_app
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+def test_home_page(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert response.data == b"Hello, World!"
